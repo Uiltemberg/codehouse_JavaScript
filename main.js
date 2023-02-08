921b346f44e708ce8ed18186b6266482d7ad6efb
Uiltemberg Duarte <uiltembergduarte@gmail.com>
2023-02-08 17:38:57 -0300

    "código para entregar em 09/02/2023"

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 let nome = prompt ("digite seu nome");
-let sobrenome = prompt("digite seu sobrenome")
-alert(nome+" "+sobrenome)
+let sobrenome = prompt("digite seu sobrenome");
+let nascimento = prompt("Digite seu ano de nascimento");
+let idade = (2023-nascimento)
+alert("Olá " +nome+ " " +sobrenome+ " você tem "+ idade+ " anos de idade.")
